42af677e17144d4dae6ecb1e72f8629b1b79dbfe
Nazeer <nazeerazam45@gmail.com>
2025-11-26 12:40:16 +0530

    Defined Task model with title, due_date, estimated_hours, importance, dependencies

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    due_date = models.DateTimeField()
+    estimated_hours = models.FloatField(validators=[MinValueValidator(0.25)])
+    importance = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    dependencies = models.JSONField(default=list, blank=True)
+
+    def __str__(self):
+        return self.title
